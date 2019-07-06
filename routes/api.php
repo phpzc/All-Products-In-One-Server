ef0f45cb503f78c4bb3d0c4db56e19361d9c6d8a
phpzc <zhang5474jj@163.com>
2019-07-06 22:55:22 +0800

    add jwt and route config

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,28 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api',
+    'middle' =>'serializer:array',
+], function($api) {
+
+    //节流处理  登录相关
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+
+    });
+
+    //节流处理 访问相关
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function($api) {
+
+    });
 });
