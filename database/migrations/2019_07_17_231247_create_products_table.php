1d408b68bcb3065c53a48587b42d46b1dcfeaa11
phpzc <zhang5474jj@163.com>
2019-07-17 23:26:30 +0800

    添加产品和服务器模型与数据库迁移文件

--- database/migrations/2019_07_17_231247_create_products_table.php
+++ database/migrations/2019_07_17_231247_create_products_table.php
@@ -19,8 +19,8 @@ class CreateProductsTable extends Migration
             $table->string('name')->comment('产品名称');
             $table->tinyInteger('type',0)->comment('产品类型 0 PC网站 1H5网站 2安卓APP 3IOS APP 4微信小程序 5可执行程序 6其他');
             $table->string('url')->comment('产品主要地址 可以填官网类地址');
-            $table->tinyInteger('status',1)->comment('产品状态  0作废 1正常 2禁止访问');
-            $table->tinyInteger('is_self',1)->comment('是否是 自由的产品 还是外包的产品 ');
+            $table->tinyInteger('status')->default(1)->comment('产品状态  0作废 1正常 2禁止访问');
+            $table->tinyInteger('is_self')->default(1)->comment('是否是 自由的产品 还是外包的产品 ');
             $table->string('remark')->nullable()->comment('产品简要说明');
 
             $table->timestamps();
